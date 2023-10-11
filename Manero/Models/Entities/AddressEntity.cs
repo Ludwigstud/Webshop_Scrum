c8fc5e813c778e5674077ac83ae2e99f2113dfe1
Ludwigstud <ludwigstridh@gmail.com>
2023-10-11 14:10:31 +0200

    Add project files.

---
+++ Manero/Models/Entities/AddressEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Manero.Models.Entities;
+
+public class AddressEntity
+{
+    public int Id { get; set; }
+    public string StreetName { get; set; } = null!;
+    public int PostalCode { get; set; }
+    public string City { get; set; } = null!;
+    public ICollection<CustomerAddressEntity> Customers { get; set; } = new HashSet<CustomerAddressEntity>();
+}
---
+++ Manero/Models/Entities/DiscountEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Manero.Models.Entities;
+
+public class DiscountEntity
+{
+    public int Id { get; set; }
+
+    public string DiscountName { get; set; } = null!;
+    public int Discount {  get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public DateTime? ExpiringDate { get; set; }
+    public ICollection<ProductEntity> Products { get; set; } = null!;
+
+}
