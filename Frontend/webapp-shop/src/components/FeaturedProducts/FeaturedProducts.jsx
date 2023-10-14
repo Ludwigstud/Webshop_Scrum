b8ed65f24fc166894d146ad02673c2998576a4e4
iiHadi66 <44447663+iiHadi66@users.noreply.github.com>
2023-10-14 15:33:13 +0200

    push to github

---
+++ Frontend/webapp-shop/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './_FeaturedProducts.scss'
+
+const FeaturedProducts = () => {
+  return (
+    <div>FeaturedProducts</div>
+  )
+}
+
+export default FeaturedProducts
---
+++ Frontend/webapp-shop/src/components/Showcase/Showcase.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './_Showcase.scss'
+
+const Showcase = () => {
+  return (
+    <div>Showcase</div>
+  )
+}
+
+export default Showcase
