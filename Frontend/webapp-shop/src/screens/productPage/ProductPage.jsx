f3b42ad1611d6ec244a69af7fd1fe822e60d1800
Ludwigstud <ludwigstridh@gmail.com>
2023-10-23 19:56:15 +0200

    some finishing touches to frontend work, backend connected and errorhandling added. no tests done yet

--- Frontend/webapp-shop/src/screens/productPage/ProductPage.jsx
+++ Frontend/webapp-shop/src/screens/productPage/ProductPage.jsx
@@ -5,13 +5,9 @@ import ProductCard from "../../components/products/ProductCard";
 import ProductReview from "../../components/products/ProductReview";
 
 const ProductPage = () => {
-  const [data, setData] = useState([]);
+  
 
-  useEffect(() => {
-    fetch("https://localhost:7042/api/Products")
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+  
 
   return (
     <>
