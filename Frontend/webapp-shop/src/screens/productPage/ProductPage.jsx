ac369da0c220fd7e49b5bf562162a291c48aabd0
Dennis Frölander <Dennisfrolander1@hotmail.com>
2023-10-24 11:25:02 +0200

    Fixed some errormessage

--- Frontend/webapp-shop/src/screens/productPage/ProductPage.jsx
+++ Frontend/webapp-shop/src/screens/productPage/ProductPage.jsx
@@ -7,11 +7,11 @@ import ProductReview from "../../components/products/ProductReview";
 const ProductPage = () => {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch("https://localhost:7042/api/Products")
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://localhost:7042/api/Products")
+  //     .then((response) => response.json())
+  //     .then((data) => setData(data));
+  // }, []);
 
   return (
     <>
