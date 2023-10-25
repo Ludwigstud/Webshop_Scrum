e7f5f3decc9bf416a20b0ea8c4027915fd5c8d10
Jonathan Johansson <111742024+gogosimba@users.noreply.github.com>
2023-10-25 12:53:21 +0200

    made the burgermenu, styling and a test.

--- Frontend/webapp-shop/src/components/topNavbar/TopNavbar.jsx
+++ Frontend/webapp-shop/src/components/topNavbar/TopNavbar.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { SlLocationPin } from 'react-icons/sl';
-import { AiOutlineMail } from 'react-icons/ai';
-import { AiOutlinePhone } from 'react-icons/ai';
+import {
+  AiOutlineMail,
+  AiOutlinePhone,
+  AiOutlineArrowRight,
+} from 'react-icons/ai';
 import './_topNavbar.scss';
 
 function Example() {
@@ -26,20 +29,34 @@ function Example() {
         <Offcanvas.Body className='offcanvas-body'>
           <h3>Contact us</h3>
           <div className='contact-icons'>
-            <i className='icon'>
-              <SlLocationPin />
+            <div className='contactinfo'>
+              <SlLocationPin className='icon' />
               <p>26 Division ST, New York, NY 100002, USA</p>
-            </i>
-            <i className='icon'>
-              <AiOutlineMail />
+            </div>
+
+            <div className='contactinfo'>
+              <AiOutlineMail className='icon' />
               <p>manerosale@mail.com</p>
               <p>manerosupport@mail.com</p>
-            </i>
-            <i className='icon'>
-              <AiOutlinePhone />
+            </div>
+
+            <div className='contactinfo'>
+              <AiOutlinePhone className='icon' />
               <p>+17 123456789</p>
               <p>+17 987654321</p>
-            </i>
+            </div>
+          </div>
+
+          <div className='order-tracker'>
+            <p>Track your order</p>
+            <div className='input-container'>
+              <input type='text' placeholder='Order ID' />
+              <button type='submit'>
+                <div className='container-arrow'>
+                  <AiOutlineArrowRight />
+                </div>
+              </button>
+            </div>
           </div>
         </Offcanvas.Body>
       </Offcanvas>
