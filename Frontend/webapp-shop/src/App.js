4e4071f5fbdf4921c0b28c7314c8f1b406c8f76f
Jonathan Johansson <111742024+gogosimba@users.noreply.github.com>
2023-10-27 11:10:24 +0200

    fixed navigation and removed temporary navbars from individual pages/screens

--- Frontend/webapp-shop/src/App.js
+++ Frontend/webapp-shop/src/App.js
@@ -2,15 +2,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BottomNavbar from './components/bottomNavbar/BottomNavbar';
 import TopNavbar from './components/topNavbar/TopNavbar';
 import ProductPage from './screens/productPage/ProductPage';
+import Profile from './screens/profile/Profile';
+import Home from './screens/Home/Home';
 
 function App() {
   return (
     <Router>
       <div className='App'>
-        <BottomNavbar />
         <Routes>
+          <Route path='/home' element={<Home />} />
           <Route path='/product' element={<ProductPage />} />
+          <Route path='/profile' element={<Profile />} />
         </Routes>
+        <BottomNavbar />
       </div>
     </Router>
   );
