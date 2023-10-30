2bb7729c75f0c42ff67572b1b6cbeab078502fea
Dennis Frölander <Dennisfrolander1@hotmail.com>
2023-10-30 16:15:06 +0100

    add html / css - add creditcard

--- Backend/Manero/Models/Entities/CustomerCardEntity.cs
+++ Backend/Manero/Models/Entities/CustomerCardEntity.cs
@@ -1,10 +1,13 @@
-﻿namespace Manero.Models.Entities;
+﻿using System.Globalization;
+
+namespace Manero.Models.Entities;
 
 public class CustomerCardEntity
 {
     public int Id { get; set; }
     public string? PaymentType { get; set; }
     public string? Provider {  get; set; }
+    public string Name { get; set; } = null!;
     public int Number {  get; set; }
     public int Money { get; set; }
     public DateTime ExpiryDate { get; set; }
