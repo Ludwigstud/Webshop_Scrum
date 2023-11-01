b7c199832d94bbff74ee6cfb204497b0870abc4e
Dennis Frölander <Dennisfrolander1@hotmail.com>
2023-11-01 12:06:33 +0100

    Add creditcard carousel and some dummy data

--- Frontend/webapp-shop/src/screens/profile/paymentMethods/paymentMethodsScreen/paymentMethodsScreen.jsx
+++ Frontend/webapp-shop/src/screens/profile/paymentMethods/paymentMethodsScreen/paymentMethodsScreen.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { BsChevronLeft, BsPlusLg, BsPencil } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import CreditCardCarousel from "../../../../components/Carousels/CreditCardCarousel/CreditCardCarousel";
 const PaymentMethodsScreen = () => {
+    const creditCards = [
+        {
+          provider: "Visa",
+          number: "4567 3214 5667 1234",
+          fullName: "Dennis Frölander",
+          expiryDate: "12/24"
+        },
+        {
+          provider: "Mastercard",
+          number: "1234 1234 1324 5678",
+          fullName: "Hannes Nilsson Tengnäs",
+          expiryDate: "10/23"
+        },
+      ];
+      const emptyCreditCards = [];
+
     return (
         <section className="payment-methods-section my-3">
             <div className="container">
@@ -17,8 +34,8 @@ const PaymentMethodsScreen = () => {
                         <h2 className="text-center">Payments method</h2>
                     </div>
                 </div>
-                <div className="row payment-methods-column d-flex justify-content-center">
-                    <div className="col-12 col-md-7 d-flex align-items-center justify-content-between py-3">
+                <div className="row payment-methods-container d-flex justify-content-center">
+                    <div className="col-12 col-md-7 d-flex align-items-center justify-content-between py-3 payment-methods-column">
                         <div className="d-flex align-items-center">
                             <h3>Cards</h3>
                         </div>
@@ -28,7 +45,10 @@ const PaymentMethodsScreen = () => {
                             </Link>
                         </div>
                     </div>
-                    <div className="col-12 col-md-7 d-flex align-items-center justify-content-between py-3">
+                    <div className="col-12 col-md-6">
+                        <CreditCardCarousel creditCards={creditCards}/>
+                    </div>
+                    <div className="col-12 col-md-7 d-flex align-items-center justify-content-between py-3 payment-methods-column">
                         <div className="d-flex align-items-center">
                             <h3>Apple Pay</h3>
                         </div>
@@ -38,7 +58,7 @@ const PaymentMethodsScreen = () => {
                             </Link>
                         </div>
                     </div>
-                    <div className="col-12 col-md-7 d-flex align-items-center justify-content-between py-3">
+                    <div className="col-12 col-md-7 d-flex align-items-center justify-content-between py-3 payment-methods-column">
                         <div className="d-flex align-items-center">
                             <h3>PayPal</h3>
                         </div>
