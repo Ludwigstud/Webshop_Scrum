12103556356ed6bf13db411c3fe00effe7adee8f
Hannesnt <hannesnt@gmail.com>
2023-11-14 10:42:16 +0100

    Css and buggfix

--- Backend/Manero/Controllers/ProductsController.cs
+++ Backend/Manero/Controllers/ProductsController.cs
@@ -28,9 +28,9 @@ public class ProductsController : ControllerBase
         try
         {
             var items = await _context.Products.ToListAsync();
-            var products = new List<Product>();
+            var products = new List<Models.dto.Product>();
             foreach (var item in items)
-                products.Add(new Product
+                products.Add(new Models.dto.Product
                 {
                     Id = item.Id,
                     ProductName = item.ProductName,
@@ -51,6 +51,33 @@ public class ProductsController : ControllerBase
 
     [HttpGet("GetproductsHadi")]
     public async Task<IActionResult> GetProductsHadi()
+    {
+        var items = await _context.Products.ToListAsync();
+        var products = new List<ProductEntity>();
+        foreach (var item in items)
+        {
+            products.Add(new ProductEntity
+            {
+                Id = item.Id,
+                ProductName = item.ProductName,
+                Price = item.Price,
+                PriceAfterSale = item.PriceAfterSale,
+                Description = item.Description,
+                ImageUrl = item.ImageUrl,
+                CategoryId = item.CategoryId
+            });
+        }
+
+        if (products.Count == 0)
+        {
+            return BadRequest();
+        }
+        else
+        {
+            return Ok(products);
+        }
+    }
+    public async Task<IActionResult> arsle()
     {
         var items = await _context.Products.ToListAsync();
         var products = new List<ProductEntity>();
