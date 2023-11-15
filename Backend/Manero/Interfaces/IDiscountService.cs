bc1d3c9f95b6e95a3efcf4027bd037b22eaf2142
Kennie92 <112747877+Kennie92@users.noreply.github.com>
2023-11-15 17:37:39 +0100

    DiscountBackened

---
+++ Backend/Manero/Interfaces/IDiscountService.cs
@@ -0,0 +1,12 @@
+﻿using Manero.Models.Entities;
+using Manero.Models.Schemas;
+
+namespace Manero.Interfaces
+{
+    public interface IDiscountService
+    {
+        Task<DiscountEntity> CreateAsync(DiscountSchema dCshema);
+        Task<DiscountEntity> DeleteAsync(Guid id);
+        Task<IEnumerable<DiscountEntity>> GetAllAsync();
+    }
+}
---
+++ Backend/Manero/Services/DiscountService.cs
@@ -0,0 +1,65 @@
+﻿using Manero.Interfaces;
+using Manero.Models.Entities;
+using Manero.Models.Schemas;
+using Manero.Repos;
+using System.Diagnostics;
+
+namespace Manero.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        private readonly IDiscountRepo _discRepo;
+
+        public DiscountService(IDiscountRepo discRepo)
+        {
+            _discRepo = discRepo;
+        }
+
+        public async Task<DiscountEntity> CreateAsync(DiscountSchema dCshema)
+        {
+            try
+            {
+                if (dCshema != null)
+                {
+                    var discountEntity = new DiscountEntity
+                    {
+                        DiscountName = dCshema.DiscountName,
+                        Discount = dCshema.Discount,
+                        ExpiringDate = dCshema.ExpiringDate
+                    };
+
+                    return await _discRepo.AddAsync(discountEntity);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return null!;
+        }
+
+
+        public async Task<IEnumerable<DiscountEntity>> GetAllAsync()
+        {
+            try
+            {
+                return await _discRepo.GetAllAsync();
+            }
+            catch (Exception ex) { Debug.WriteLine(ex.Message); }
+            return null!;
+        }
+
+        public async Task<DiscountEntity> DeleteAsync(Guid id)
+        {
+            try
+            {
+                return await _discRepo.DeleteAsync(id);
+            }
+            catch (Exception ex) { Debug.WriteLine(ex.Message); }
+            return null!;
+        }
+
+       
+    }
+}
